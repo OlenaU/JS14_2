306eeb0c3a13caaba92be6d80106c0dc98c291ec
OlenaU <udalaolena@gmail.com>
2022-01-26 03:12:41 -0800

    drafts - nt to loose

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+/*function code(){
+
+let inputcode = document.getElementById('xvar').value;
+console.log(inputcode);
+
+let codeString = inputcode;
+var symbols = codeString.split("");
+console.log(symbols);
+
+let arricao = [Alfa, Bravo, Charlie, Delta, Echo, Foxtrot, Golf, Hotel, India, Juliett, Kilo, Lima, Mike, November, Oscar, Papa, Quebec, Romeo, Sierra, Tango, Uniform, Victor, Whiskey, X-ray, Yankee, Zulu, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+let filteralph = arricao.filter(code => {
+    arricao[0] === symbols[0];
+  });
+  console.log(filteralph);
+
+}
+
+
+
+/*
+//arralphNumb = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+arricao = [Alfa, Bravo, Charlie, Delta, Echo, Foxtrot, Golf, Hotel, India, Juliett, Kilo, Lima, Mike, November, Oscar, Papa, Quebec, Romeo, Sierra, Tango, Uniform, Victor, Whiskey, X-ray, Yankee, Zulu, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+let filteralph = arricao.filter(code => {
+    arricao[0] === document.getElementById('xvar').value;
+  });
+  console.log(filteralph);
+  
+  filteredList;
+
+/*A : Alfa
+B : Bravo
+C - Charlie
+D - Delta
+E - Echo
+F - Foxtrot
+G - Golf
+H - Hotel
+I - India
+J - Juliett
+K - Kilo
+L - Lima
+M - Mike
+N - November
+O - Oscar
+P - Papa
+Q - Quebec
+R - Romeo
+S - Sierra
+T - Tango
+U - Uniform
+V - Victor
+W - Whiskey
+X - X-ray
+Y - Yankee
+Z - Zulu​ */
